4f9420ef952068cdaff9d6cb9cc0c9ea88953386
0x11a41 <harikrishnamohan@proton.me>
2026-07-06 00:27:08 +0530

    first server

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 
 #include "include/itypes.h"
 #include "include/utils.h"
@@ -15,8 +16,27 @@ static void* handle_request(void *void_fd)
 {
     i32 fd = *(i32 *)void_fd;
     free(void_fd);
+
+    char buffer[2048];
+    
+    ssize_t bytes = recv(fd, buffer, sizeof(buffer) - 1, 0);
+    if (bytes > 0) {
+        buffer[bytes] = '\0';
+        
+        const char *response = 
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 18\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "Hello from Thread!";
+
+        send(fd, response, strlen(response), 0);
+    }
+    close(fd);
     return NULL;
 }
+
 
 i32 main()
 {
@@ -35,40 +55,36 @@ i32 main()
     assert(bind(serv_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) != -1);
     assert(listen(serv_fd, SOMAXCONN) != -1);
 
-    printf("server running on port %d\n", PORT);
-
-    pthread_attr_t attr = {0};
+    pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-    u32 stack_size = KB(256);
-    if (stack_size < PTHREAD_STACK_MIN) stack_size = PTHREAD_STACK_MIN;
-    pthread_attr_setstacksize(&attr, stack_size);
+    pthread_attr_setstacksize(&attr, KB(256));
 
-    printf("%d\n", PTHREAD_STACK_MIN);
+    printf("http server running on port %d\n", PORT);
 
-    // for (;;) {
-    //     struct sockaddr_in client_addr = {0};
-    //     socklen_t addrlen = sizeof(client_addr);
+    for (;;) {
+        struct sockaddr_in client_addr = {0};
+        socklen_t addrlen = sizeof(client_addr);
 
-    //     i32 client_fd = accept(serv_fd, (struct sockaddr*)&client_addr, &addrlen);
-    //     if (client_fd == -1) {
-    //         perror("Accept failed");
-    //         continue;
-    //     }
+        i32 client_fd = accept(serv_fd, (struct sockaddr*)&client_addr, &addrlen);
+        if (client_fd == -1) {
+            perror("Accept failed");
+            continue;
+        }
 
-    //     i32 *exclusive_fd = malloc(sizeof(i32));
-    //     assert(exclusive_fd != NULL);
-    //     *exclusive_fd = client_fd;
+        i32 *exclusive_fd = malloc(sizeof(i32));
+        assert(exclusive_fd != NULL);
+        *exclusive_fd = client_fd;
         
-    //     pthread_t tid;
+        pthread_t tid;
 
-    //     if (pthread_create(&tid, &attr, handle_request, exclusive_fd) != 0) {
-    //         perror("Failed to create thread");
-    //         close(client_fd);
-    //         free(exclusive_fd);
-    //     }
+        if (pthread_create(&tid, &attr, handle_request, exclusive_fd) != 0) {
+            perror("Failed to create thread");
+            close(client_fd);
+            free(exclusive_fd);
+        }
 
-    // }
+    }
 
     pthread_attr_destroy(&attr);
     shutdown(serv_fd, SHUT_RD);
